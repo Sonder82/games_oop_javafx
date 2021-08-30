8e55dd909f8463e07748609fc6984360532c9ff5
Sonder82 <Himrs82@gmail.com>
2021-08-30 19:32:26 +0500

    2. Сокобан. Тестовое задание. [#75283]

--- puzzle/src/test/java/ru/job4j/puzzle/WinTest.java
+++ puzzle/src/test/java/ru/job4j/puzzle/WinTest.java
@@ -6,7 +6,8 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
-@Ignore
+
+
 public class WinTest {
     @Test
     public void whenVerticalWin() {
